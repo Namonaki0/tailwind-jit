87496b2782ed9071e3cb2293bfa78aefdcec6c53
Andre <andreferreira@live.co.uk>
2023-07-03 11:08:25 +0100

    extra variants and arbitrary values

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  mode: "JIT",
+  content: ["./*.html"],
+  components: ["./*.{html, css}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
